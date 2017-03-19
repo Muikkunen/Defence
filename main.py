97413ca7bf781fb2e5c8e314336930e35b100edf
Samuli Mononen <s.m@phnet.fi>
2017-03-19 16:07:40 +0200

    Minor modifications

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from setup import Setup
 def main():
 	file_name = "Game_data_1.0.txt"
 
-	Setup().load_game(file_name)
-	
+	Setup().load_all(file_name)
+
 
 main()
--- tower.py
+++ tower.py
@@ -1,2 +1,27 @@
-class tower():
+import coordinates
 
+class Tower(object):
+
+	def __init__ (self, location, damage, shoot_range, relay_time, build_time):
+		self.location = location
+		self.damage = damage
+		self.shoot_range = shoot_range
+		self.relay_time = relay_time
+		self.build_time = build_time
+
+"""
+	def enemy_in_range(self, enemys):
+		for enemy in enemys:
+			if distance(enemy.get_location(), location) <= shoot_range:
+				self.shoot(enemy.get_location())
+
+	def shoot(self, target):
+		missile = Missile(target)
+		game.add_missile(missile, target)
+
+
+def main():
+	tower = Tower((1, 1), 100, 10, 10, 10)
+
+
+main()"""
