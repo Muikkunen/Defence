02a2a917951855249715f428b1a10f1cdbde3ab3
Samuli Mononen <s.m@phnet.fi>
2017-03-21 10:18:53 +0200

    Added printing game field as text

--- main.py
+++ main.py
@@ -6,7 +6,18 @@ from setup import Setup
 def main():
 	file_name = "Game_data_1.0.txt"
 
-	Setup().load_all(file_name)
+	game = Setup().load_all(file_name)
+
+	
+	for j in range(game.board.get_height()):
+		print("")
+		for i in range(game.board.get_width()):
+			if game.board.squares[i][j].contains() == 0:
+				print("#", end="")
+			elif game.board.squares[i][j].contains() == 1:
+				print(" ", end="")
+
+	
 
 
 main()
