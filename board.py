559a30a74b186b2ccc02e8b633b40db37dce57e1
Samuli Mononen <s.m@phnet.fi>
2017-03-21 10:19:27 +0200

    Board can now setup route according to the destination points

--- board.py
+++ board.py
@@ -6,7 +6,6 @@ from square import Square
 
 class Board(object):
 
-
 	def __init__ (self, width, height):
 
 		self.squares = [None] * width
@@ -16,6 +15,7 @@ class Board(object):
 				self.squares[i][j] = Square()
 
 		# Containers
+		self.towers = []
 		self.enemys = []
 		self.missiles = []
 		self.route_points = []
@@ -31,21 +31,73 @@ class Board(object):
 		return len(self.squares[0])
 
 
-	def add_tower(self, tower, position):
-		# Adds tower to the board
-		if self.squares[position[0]][position[1]].is_empty():
-			self.squares[position[0]][position[1]].set_tower(tower)
+	def add_tower(self, position, tower):
+		# Adds tower to the board if the certain square is empty
+		if self.squares[position[0]][position[1]].contains() == 0:
+			# Add tower to the container list
+			self.towers.append(tower)
+			# Set Square's type to tower
+			self.squares[position[0]][position[1]].set_tower()
 		else:
 			print("CANNOT ADD")													#----------------------- NOT IMPLEMENTED
 
 
 	def add_destination(self, position):
 		# Adds next destination for enemys to the board
-		if self.squares[int(position[0])][int(position[1])].is_empty():
+		if self.squares[int(position[0])][int(position[1])].contains() == 0:
 			self.route_points.append(position)
 
 		else:
 			print("CANNOT ADD")													#----------------------- NOT IMPLEMENTED
+
+
+	def add_route(self):
+		amount_of_route_points = len(self.route_points)
+
+		# Set first route point to route here because the following loop will not set the first square to route
+		#	to prevent setting square that has already been set to square
+		self.squares[self.route_points[0][0]][self.route_points[0][1]].set_route()
+
+		# Iterate through route points until second last to prevent checking last value with one that doesn't exist
+		for i in range(amount_of_route_points - 1):
+			self.squares[self.route_points[i + 1][0]][self.route_points[i + 1][1]].set_route()
+
+			print("x: {} y: {}".format(self.route_points[i][0], self.route_points[i][1]))
+
+			# If consecutive route points have the same x-coordinate set the vertical squares between first and second point to route
+			if self.route_points[i][0] == self.route_points[i + 1][0]:
+
+				# Check which route point's y value is greater
+				if self.route_points[i][1] < self.route_points[i + 1][1]:
+					loop_direction = 1
+				else:
+					loop_direction = -1
+
+				for j in range(self.route_points[i][1] + loop_direction, self.route_points[i + 1][1], loop_direction):
+					if self.squares[self.route_points[i][0]][j].contains() == 0:
+						self.squares[self.route_points[i][0]][j].set_route()
+					else:
+						print("CANNOT ADD")										#----------------------- NOT IMPLEMENTED
+
+			# If consecutive route points have the same y-coordinate set the horizontal squares between first and second point to route
+			elif self.route_points[i][1] == self.route_points[i + 1][1]:
+
+				# Check which route point's x value is greater
+				if self.route_points[i][0] < self.route_points[i + 1][0]:
+					loop_direction = 1
+				else:
+					loop_direction = -1
+
+				for j in range(self.route_points[i][0] + loop_direction, self.route_points[i + 1][0], loop_direction):
+					if self.squares[j][self.route_points[i][1]].contains() == 0:
+						self.squares[j][self.route_points[i][1]].set_route()
+					else:
+						print("CANNOT ADD")										#----------------------- NOT IMPLEMENTED
+
+
+			# If route points have the same y-coordinate set the horizontal squares between first and second point to route
+			#elif first_route_point[1] == second_route_point[1]:
+
 
 
 	"""def remove_tower(self, tower):
