9e4ed5b51c04ec4bd7a3ad06a0f23a06770a631d
Samuli Mononen <s.m@phnet.fi>
2017-04-11 21:28:36 +0300

    More testing implemented

--- main.py
+++ main.py
@@ -8,19 +8,40 @@ def main():
 
 	game = Setup().load_all(file_name)
 
-	
+	#game.board.current_wave(game.board.waves[0])
+
+	game.get_board().set_route_points(game.routes[1][1])
+	game.get_board().add_route()
+
 	for j in range(game.board.get_height()):
 		print("")
 		for i in range(game.board.get_width()):
 			if game.board.squares[i][j].contains() == 0:
-				print("#", end="")
+				print(".", end="")
 			elif game.board.squares[i][j].contains() == 1:
 				print(" ", end="")
+	
+	game.get_board().get_enemies().append(game.get_board().waves[0][0])
+	game.get_board().get_enemies().append(game.get_board().waves[0][1])
 
-	#game.board.current_wave(game.board.waves[0])
+	game.get_board().get_enemies()[0].set_location(list(game.get_board().get_route_points()[0]))
 
-	for i in range(5):
-		print(game.board.waves[0][1].name)
+	while 0 < game.get_lives():
+		list_of_enemies_to_be_killed = []
+		if game.get_board().get_enemies() == []:
+			break
+		for enemy in game.get_board().get_enemies():
+			print(enemy.location)
+			if enemy.move(enemy.get_speed()):
+				list_of_enemies_to_be_killed.append(enemy)
+			print("Player's lives: {}".format(game.get_lives()))
+
+		for enemy in list_of_enemies_to_be_killed:
+			game.get_board().kill_enemy(enemy)
+
+	
+
+	print(game.get_board().get_enemies())
 	
 
 
