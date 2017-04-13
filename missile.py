d7052212c6cf6850a11a928da0c2d32a022b406e
Samuli Mononen <s.m@phnet.fi>
2017-04-13 19:35:08 +0300

    Implemented basics for missile

--- missile.py
+++ missile.py
@@ -3,3 +3,8 @@ class Missile(object):
 	def __init__ (self, missile_type, speed):
 		self.missile_type = missile_type
 		self.speed = speed
+
+
+	def initialize(self, target, damage):		# Set the target and damage for missile
+		self.target = target
+		self.damage = damage
